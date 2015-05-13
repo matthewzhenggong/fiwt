5c58a8278c14a24c9d584b346e6494b17a3a2ccd
Matthew GONG <matthewzhenggong@gmail.com>
2015-05-13 09:27:09 +0100

    comm funcs different impl in win/liunux

--- FIWT-APC/utils.py
+++ FIWT-APC/utils.py
@@ -31,7 +31,16 @@ if is_windows:
 
     def getMicroseconds():
         return int(time.clock() * 1e6)
+
+    def getBindHost(host):
+        return (host,host)
 else:
+    import netaddr
 
     def getMicroseconds():
         return int(time.time() * 1e6)
+
+    def getBindHost(host):
+        net = netaddr.IPNetwork(host+'/24')
+        return (str(net.network), str(net.broadcast))
+
